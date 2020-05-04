96978c1bb025627c4be88563914ffa1f1bbe3c6a
Rupesh Prajapati <39094963+Rupeshpra22@users.noreply.github.com>
2020-05-04 14:59:42 +0530

    Added Resume Section and Changed OtherInfo to Contacts

--- index.js
+++ index.js
@@ -29,14 +29,8 @@ function scrollAppear() {
     var reactbar = document.querySelector('.reactbar');
     var dotnetbar = document.querySelector('.dotnetbar');
     var sqlbar = document.querySelector('.sqlbar');
-    var programming = document.querySelector('.others .programming');
-    var personal = document.querySelector('.others .personal')
-    var socailNetwork = document.querySelector('.others .social-network')
-    //below line will return the distance of aboutText class element from top
     var aboutPosition = aboutText.getBoundingClientRect().top;
     var skillposition = htmlcssbar.getBoundingClientRect().top;
-    var contactsPosition = programming.getBoundingClientRect().top;
-    //window.innerHeight will return the height of your screen
     var screenPosition = window.innerHeight / 1.2;
     if (aboutPosition < screenPosition) {
         aboutText.classList.add('animation');
@@ -48,11 +42,6 @@ function scrollAppear() {
         dotnetbar.classList.add('fill-dotnet');
         sqlbar.classList.add('fill-sql');
         skills.classList.add('animation');
-    }
-    if (contactsPosition < screenPosition) {
-        programming.classList.add('handles-animation');
-        personal.classList.add('handles-animation');
-        socailNetwork.classList.add('handles-animation')
     }
 }
 
